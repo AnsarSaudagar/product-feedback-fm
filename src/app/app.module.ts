06be6ea76c8aa79ebd757313f04f405e9d043a03
Ansar <ansarsaudagar40@gmail.com>
2023-10-24 14:27:06 +0530

    checking the new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RoadMapComponent } from './road-map/road-map.component';
 import { FeedbackComponent } from './feedback/feedback.component';
+import { CheckComponent } from './check/check.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     RoadMapComponent,
-    FeedbackComponent
+    FeedbackComponent,
+    CheckComponent
   ],
   imports: [
     BrowserModule,
